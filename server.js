06946ce070ecc07f2bcafbac260dfa9d40820716
OmkarSsawant <omkar18sawant@gmail.com>
2021-10-25 14:23:19 +0530

    added md

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const api = require('./tech_api')
 const app = express()
 const TECH_MONGO_URI = "mongodb+srv://omkar-sawant:Ewn9vwJ5DUnnK4LG@cluster0.iyx7h.mongodb.net/tech?retryWrites=true&w=majority";
 
-//TODO: integrate affiliate api here
+
 
 const mongoose = require('mongoose')
 mongoose.connect(TECH_MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(_ => console.log("Connted DB 💿"))
---
+++ todos.md
@@ -0,0 +1,6 @@
+> integrate affiliate api here
+
+> optimize queries to get specific results
+
+> integrate product comparison api
+
